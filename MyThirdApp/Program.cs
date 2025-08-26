1dd17e7a8428ec53244a6819bbac47c0db7201dd
gbertoso <guilhermedasilvabertoso@gmail.com>
2025-08-26 14:09:04 -0300

    Initial commit

---
+++ MyThirdApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace MyThirdApp;
+
+public class MyThirdApp
+{
+    class Task
+    {
+        private string name = "TaskName";
+        private string description = "Task";
+    }
+
+    static void Main(string[] args)
+    {
+        List<string> tasks = new List<string>();
+        if (File.Exists("tasks_save.json"))
+        {
+            Console.WriteLine("The save exists!");
+        }
+        else
+        {
+            Console.WriteLine("The save does not exist!");
+            Console.Write("Do you want to create a tasks save?");
+            string response = Console.ReadLine();
+            if (response == "y" && response == "yes" && response == "Y" && response == "Yes")
+            {
+                string json_file = JsonSerializer.Serialize(tasks);
+                File.WriteAllText("tasks_save.json", json_file);
+            }
+            Console.WriteLine("Created a tasks save!");
+        }
+    }
+}
